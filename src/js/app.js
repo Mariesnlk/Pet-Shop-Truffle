7f885a1098c4891113cd4e8637ec056efbf116fb
Mariia Synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2022-01-26 18:45:29 +0200

    interact ui with sc

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,15 @@ App = {
   web3Provider: null,
   contracts: {},
 
-  init: async function() {
+  init: async function () {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
+    $.getJSON('../pets.json', function (data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
+      let i;
+
+      for (i = 0; i < data.length; i++) {
         petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
         petTemplate.find('.pet-breed').text(data[i].breed);
@@ -23,46 +25,105 @@ App = {
     return await App.initWeb3();
   },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+  initWeb3: async function () {
+    // modern dapp browser for more recent version of MetaMask 
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // request account access
+        await window.ethereum.request({ methid: "eth_requestAccounts" });
+      } catch (error) {
+        console.error("User denied account access")
+      }
+    }
+
+    // legacy dapp browser
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+
+    // if no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider("http://localhost:7545");
+    }
+
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+  initContract: function () {
+    $.getJSON("Adoption.json", function (data) {
+      //get the necessary contract artifact file and instantiate it with @truffle/contract
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      //set the provider for the contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      //use the contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
 
     return App.bindEvents();
   },
 
-  bindEvents: function() {
+  bindEvents: function () {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
+  markAdopted: function () {
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function (instance) {
+      adoptionInstance = instance;
+
+      //use Call
+      return adoptionInstance.getAdopters.call();
+    }).then(function (adopters) {
+      for (let i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== "0x0000000000000000000000000000000000000000") {
+          $(".panel-pet").eq(i).find("button").text("Success").attr("disabled", true);
+        }
+      }
+    }).catch(function (err) {
+      console.log(err.message);
+    });
+
   },
 
-  handleAdopt: function(event) {
+  handleAdopt: function (event) {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    // get user`s account
+    web3.eth.getAccounts(function (error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      // select the fisrt account
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function (instance) {
+        adoptionInstance = instance;
+
+        return adoptionInstance.adopt(petId, { from: account });
+      }).then(function (result) {
+        return App.markAdopted();
+      }).catch(function (err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
 
-$(function() {
-  $(window).load(function() {
+$(function () {
+  $(window).load(function () {
     App.init();
   });
 });
